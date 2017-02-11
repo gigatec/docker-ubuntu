df256e2342ff5bc47f4a24e8312510bdab3453f3
Stefan Kreuter <kreuter@gigatec.de>
2017-02-11 20:11:53 +0100

    update Dockerfile.ubuntu-16.04-php5.6-dev

--- php/init.sh
+++ php/init.sh
@@ -7,31 +7,11 @@ ENVIRONMENT="${3:-prod}"
 # Ignore the post install questions
 export DEBIAN_FRONTEND=noninteractive
 
-echo '-- Update Apt --'
-apt-get update
-
-echo "-- Install tools and helpers --"
-apt-get install -y software-properties-common
-
-echo "-- Install PPA's --"
-add-apt-repository ppa:ondrej/php
-
-echo "-- Get PPA ondrej Key --"
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
-
-echo '-- Update & Upgrade Apt --'
-apt-get update
-apt-get upgrade
-
 if [ "$ENVIRONMENT" = "dev" ]; then
 	echo '-- Install Dev Tools --'
 	apt-get install -y \
 		vim git dos2unix zip wget lynx curl
 fi
-
-echo '-- Install sudo & Apache --'
-apt-get install -y \
-	sudo apache2
 
 echo "-- Install PHP ${PHP_VERSION} --"
 apt-get install -y \
@@ -47,9 +27,6 @@ fi
 
 phpenmod mcrypt
 
-# Change Apache settings (User: vagrant, Group: vagrant)
-sed -i 's/www-data/vagrant/g' /etc/apache2/envvars
-
 # Change PHP settings (short_open_tag, ...)
 sed 's/short_open_tag = Off/short_open_tag = On/g' -i /etc/php/${PHP_VERSION}/apache2/php.ini
 sed 's/^post_max_size.*$/post_max_size = 50M/g' -i /etc/php/${PHP_VERSION}/apache2/php.ini
@@ -57,36 +34,10 @@ sed 's/^upload_max_filesize.*$/upload_max_filesize = 50M/g' -i /etc/php/${PHP_VE
 sed 's/^max_execution_time = .*/max_execution_time = 240/g' -i /etc/php/${PHP_VERSION}/apache2/php.ini
 sed 's/^; max_input_vars = .*/max_input_vars = 1500/g' -i /etc/php/${PHP_VERSION}/apache2/php.ini
 
-# Delete default apache web dir and symlink mounted vagrant dir from host machine
-# --------------------
-rm -rf /var/www/html
-ln -fs /vagrant/public /var/www/html
-
 echo '-- Install Composer --'
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/bin/composer
 chmod +x /usr/bin/composer
-
-# Replace contents of default Apache vhost
-# --------------------
-cat > /etc/apache2/sites-enabled/000-default.conf <<EOF
-NameVirtualHost *:8080
-Listen 8080
-<VirtualHost *:80>
-  DocumentRoot "/var/www/html"
-  ServerName localhost
-  <Directory "/var/www/html">
-    AllowOverride All
-  </Directory>
-</VirtualHost>
-<VirtualHost *:8080>
-  DocumentRoot "/var/www/html"
-  ServerName localhost
-  <Directory "/var/www/html">
-    AllowOverride All
-  </Directory>
-</VirtualHost>
-EOF
 
 if [ "$ENVIRONMENT" = "dev" ]; then
 	echo '-- Activate XDebug --'
@@ -98,33 +49,6 @@ xdebug.remote_port=9000
 xdebug.remote_handler="dbgp"
 EOF
 fi
-
-a2enmod rewrite
-
-# Mysql
-# --------------------
-# Install MySQL quietly
-echo 'Install MySQL...'
-apt-get -q -y install mysql-server
-
-# Activate mysql root user from outside
-mysql -u root <<EOF
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '';
-FLUSH PRIVILEGES;
-EOF
-
-# Custom Linux/Bash settings
-echo 'alias dir="ls -al"' >> /etc/profile
-echo 'alias root="sudo -i"' >> /etc/profile
-echo 'cd /vagrant/public' >> /etc/profile
-
-# Install imagemagick
-echo 'Install imagemagick...'
-apt-get install -y imagemagick libmagickwand-dev
-
-# install mailling softtware
-echo 'Install mailling software'
-apt-get install -y postfix
 
 if [ "$ENVIRONMENT" = "dev" ]; then
 	# install imap + roundcube
